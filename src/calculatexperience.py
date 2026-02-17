839a4c42333d6302d68793a0596f1155c88eafec
xfac11 <xfac115@gmail.com>
2026-02-17 09:15:48 +0100

    Created ExerciseExperience class that has all the multipliers. Create one with the time in minutes and apply each multipliers and then get the experience points.

--- src/calculatexperience.py
+++ src/calculatexperience.py
@@ -1,20 +1,151 @@
 import math
-CONSTANT = 0.11
-SLEEP_CONSTANT = 100
-def calculate_exercise_experience(repeats:int=0, weight:float = 1, distance:float=0, time_minutes:float=0) -> int:
-    if distance == 0:
-        otherXP = (repeats * 5 * weight)
-        return int(otherXP)
-    avg_speed_kmh = distance / (time_minutes / 60)
-    cardioXP = max((distance * (time_minutes/60) * avg_speed_kmh), avg_speed_kmh*10)
-    return cardioXP
+GLOBAL_CONSTANT = 0.35
+BODY_CONSTANT = 0.25
+SCALING_INTENSITY = 0.03
+BASE_RATE = 10 ## xp per minute
+SLEEP_STREAK_CAP = 0.1
+SLEEP_STREAK_RATE = 0.01
+def clamp(number, smallest, largest): 
+    return max(smallest, min(number, largest))
+
+class ExerciseExperience:
+    def __init__(self, time_minutes, base_rate = BASE_RATE):
+        self.__exercise_xp = time_minutes * base_rate
+        self.__intensity_multiplier = 1.0
+        self.__sleep_multiplier = 1.0
+        self.__strength_multiplier = 1.0
+        self.__strength_record_multiplier = 1.0
+        self.__effort_multiplier = 1.0
+        self.__exercise_multiplier = 1.0
+        self.__multiplier = 1.0
+    
+    ##Gives a multipler from 1.0 (means 0% bonus) to 2.0 means 100% bonus
+    def apply_intensity_multiplier(self, user_speed, exercise_avg_speed, scaling_intensity = SCALING_INTENSITY):
+        relative_intensity = (user_speed - exercise_avg_speed) / exercise_avg_speed
+        relative_intensity = clamp(relative_intensity, 0.0, 1.0) 
+        
+        intensity_m = 1 + relative_intensity * scaling_intensity
+        
+        self.__multiplier *= intensity_m
+        self.__intensity_multiplier = intensity_m
+        self.__exercise_xp *= intensity_m
+
+        return self
+    
+    ## Gives a multiplier from 1.0 to sleep_streak_cap. Each streak day inreaces the multiplier by sleep_streak_rate
+    def apply_sleep_multiplier(self, sleep_streak:int, sleep_streak_rate = SLEEP_STREAK_RATE, sleep_streak_cap = SLEEP_STREAK_CAP):
+        sleep_m = 1 + min(sleep_streak * sleep_streak_rate, sleep_streak_cap)
+
+        self.__multiplier *= sleep_m
+        self.__sleep_multiplier = sleep_m
+        self.__exercise_xp *= sleep_m
+
+        return self
+    
+    def apply_strength_multiplier(self, weight, reps, sets, average_volume):
+        volume = weight * reps * sets
+        relative_volume =  volume / average_volume
+
+        improvement = max(relative_volume - 1.02, 0)
+
+        strength_bonus = min(improvement * 0.25, 0.05)
+
+        strength_m = 1 + strength_bonus
+        
+        self.__multiplier *= strength_m
+        self.__strength_multiplier = strength_m
+        self.__exercise_xp *= strength_m
+
+        return self
+    
+    def apply_strength_record_multiplier(self, weight, highest_weight):
+        if weight > highest_weight:
+            self.__strength_record_multiplier = 1.05
+            self.__exercise_xp *= 1.05
+    
+    def apply_exercise_multiplier(self, exercise_multiplier = 1.0):
+        self.__exercise_multiplier = exercise_multiplier
+        self.__multiplier *= exercise_multiplier
+        self.__exercise_xp *= exercise_multiplier
+
+        return self
+    
+    def apply_effort_multiplier(self, effort_multiplier = 1.0):
+        self.__effort_multiplier = effort_multiplier
+        self.__multiplier *= effort_multiplier
+        self.__exercise_xp *= effort_multiplier
+
+        return self
+    
+    def get_experience_points(self):
+        return self.__exercise_xp
+    
+    def get_multiplier(self):
+        return self.__multiplier
+    
+    def get_effort_multiplier(self):
+        return self.__effort_multiplier
+    
+    def get_exercise_multiplier(self):
+        return self.__exercise_multiplier
+    
+    def get_sleep_multiplier(self):
+        return self.__sleep_multiplier
+    
+    def get_strength_multiplier(self):
+        return self.__strength_multiplier
+    
+    def get_intensity_multiplier(self):
+        return self.__intensity_multiplier
+    
+    def get_strength_record_multiplier(self):
+        return self.__strength_record_multiplier
+
+def create_exercise_experience(sleep_streak = 1,
+                                  repeats:int=0,
+                                  sets=0,
+                                  weight:float = 1,
+                                  average_volume = 60,
+                                  highest_weight = 100,
+                                  distance:float=0,
+                                  time_minutes:float=0,
+                                  effort_multiplier = 1.0,
+                                  exercise_factor = 1.0,
+                                  exercise_avg_speed_kmh = 20) -> ExerciseExperience:
+    
+    user_speed = distance / (time_minutes / 60)
+
+    exercise_exp = ExerciseExperience(time_minutes, BASE_RATE)
+
+    exercise_exp.apply_effort_multiplier(effort_multiplier)
+    exercise_exp.apply_exercise_multiplier(exercise_factor)
+    exercise_exp.apply_intensity_multiplier(user_speed, exercise_avg_speed_kmh, SCALING_INTENSITY)
+    exercise_exp.apply_sleep_multiplier(sleep_streak, SLEEP_STREAK_RATE, SLEEP_STREAK_CAP)
+    exercise_exp.apply_strength_multiplier(weight, repeats, sets, average_volume, highest_weight)
+
+    return exercise_exp
     
 
-def calculate_sleep_experience(sleep_streak:int) -> int:
-    return sleep_streak*SLEEP_CONSTANT
+def calculate_global_level(current_xp:int):
+    return math.floor(GLOBAL_CONSTANT * math.sqrt(current_xp))
 
-def calculate_level(current_xp:int) -> int:
-    return CONSTANT * math.sqrt(current_xp)
+def calculate_body_level(current_xp:int):
+    return math.floor(BODY_CONSTANT * math.sqrt(current_xp))
 
-def calculate_experience(current_level:int) -> int:
-    return (current_level / CONSTANT)**2
+def calculate_body_experience(current_level:int): ##Never use for storing or for displaying current xp
+    return (current_level / BODY_CONSTANT)**2
+
+def calculate_global_experience(current_level:int): ##Never use for storing or for displaying current xp
+    return (current_level / GLOBAL_CONSTANT)**2
+
+def calculate_body_progress_to_next(xp):
+    level = calculate_body_level(xp)
+    current_level_xp = calculate_body_experience(level)
+    next_level_xp = calculate_body_experience(level + 1)
+    return (xp -  current_level_xp) / (next_level_xp - current_level_xp)
+
+def calculate_global_progress_to_next(xp):
+    level = calculate_global_level(xp)
+    current_level_xp = calculate_global_experience(level)
+    next_level_xp = calculate_global_experience(level + 1)
+    return (xp -  current_level_xp) / (next_level_xp - current_level_xp)
--- tests/test_experiencepoints.py
+++ tests/test_experiencepoints.py
@@ -1,23 +1,64 @@
 import unittest
 
-from src.calculatexperience import calculate_exercise_experience
-from src.calculatexperience import calculate_sleep_experience
+from src.calculatexperience import *
 
 class TestExperiencePoints(unittest.TestCase):
     def test_30_repeats(self):
-        self.assertEqual(calculate_exercise_experience(10, 6, 0, 0), 300)
+        exercise_exp = ExerciseExperience(30)
+        exercise_exp.apply_strength_multiplier(2, 10, 3, 40)
+        exercise_exp.apply_strength_record_multiplier(2, 6)
+        self.assertEqual(exercise_exp.get_experience_points(), 315)
     
     def test_30min_cardio(self):
-        self.assertEqual(calculate_exercise_experience(0, 0, 10, 20), 300)
+        self.assertEqual(ExerciseExperience(30).apply_intensity_multiplier(15, 20).apply_effort_multiplier().get_experience_points(), 300)
     
     def test_cardio_and_repeats(self):
-        cardio_xp = calculate_exercise_experience(0, 0, 5, 30)
-        repeats_xp = calculate_exercise_experience(45)
-        self.assertEqual(cardio_xp+repeats_xp, 100+225)
+        cardio_xp = ExerciseExperience(30)
+        
+        repeats_xp = ExerciseExperience(15)
+        repeats_xp.apply_strength_multiplier(2, 15, 15, 100)
+        repeats_xp.apply_strength_record_multiplier(2, 6)
+
+        self.assertEqual(cardio_xp.get_experience_points()+repeats_xp.get_experience_points(), 100+225)
     
     def test_sleep_experience_2(self):
-        self.assertEqual(calculate_sleep_experience(2), 200)
+        exercise_exp = ExerciseExperience(30)
+        exercise_exp.apply_sleep_multiplier(2)
+
+        self.assertEqual(exercise_exp.get_sleep_multiplier(), 1.02)
     
     def test_sleep_experience_0(self):
-        self.assertEqual(calculate_sleep_experience(0), 0)
+        exercise_exp = ExerciseExperience(30)
+        exercise_exp.apply_sleep_multiplier(0)
 
+        self.assertEqual(exercise_exp.get_sleep_multiplier(), 1.0)
+    
+    def test_getting_level_90000xp(self):
+        current_xp = 90000
+        level = calculate_body_level(current_xp)
+        self.assertEqual(level, 33)
+    
+    def test_getting_level_8264xp(self):
+        current_xp = 8265
+        level = calculate_body_level(current_xp)
+        self.assertEqual(level, 10)
+    
+    def test_getting_level_13495xp(self):
+        current_xp = 13495
+        level = calculate_body_level(current_xp)
+        self.assertEqual(level, 12)
+    
+    def test_getting_level_xp_743(self):
+        current_xp = 744
+        level = calculate_body_level(current_xp)
+        self.assertEqual(level, 3)
+    
+    def test_xp_progress(self):
+        xp = 650
+        progress = calculate_body_progress_to_next(xp)
+        self.assertGreater(progress, 0.5)
+    
+    def test_xp_progress_340(self):
+        xp = 340
+        progress = calculate_body_progress_to_next(xp)
+        self.assertLess(progress, 0.5)
