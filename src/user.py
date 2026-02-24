468a44ee27f3d3436a2eb5e40c0d14de2bf83d98
xfac11 <xfac115@gmail.com>
2026-02-24 18:22:59 +0100

    - User using increase global and body xp functions and increaseing/decreasing sleep streak and update highest weight. - test_user: testing all functions and variables.

--- src/user.py
+++ src/user.py
@@ -1,16 +1,57 @@
+from src.bodyparts import BodyParts
+from src.calculatexperience import calculate_body_level
+from src.calculatexperience import calculate_global_level
 class User:
-    def __init__(self, id:int, name:str, global_xp:int, body_xp:dict, streak:int, sleep_streak:int, history:list, highest_weight):
+    def __init__(self, id:int, name:str, global_xp:int, body_xp:BodyParts, streak:int, sleep_streak:int, log_id:list, highest_weight:int):
         self.name = name
         self.id = id
         self.global_xp = global_xp
         self.body_xp = body_xp
         self.streak = streak
         self.sleep_streak = sleep_streak
-        self.history = history
+        self.log_id = log_id
         self.highest_weight = highest_weight
 
     def repr():
         pass
-
-    def increase_xp(xp) -> bool:
-        pass
+    
+    """
+    returns 0 if no level was gained after increasing xp
+    returns the level if a level was gained after increasing xp
+    """
+    def increase_global_xp(self, xp:int) -> int:
+        level = calculate_global_level(self.global_xp)
+        self.global_xp += xp
+        level_after = calculate_global_level(self.global_xp)
+        
+        if level_after > level:
+            return level_after
+        return 0
+        
+    """
+    returns the level if it increased otherwise returns 0
+    returns -1 if the bodypart does not exist
+    """
+    def increase_body_xp(self, bodypart:str, xp:int) -> int:
+        xp_before = self.body_xp.get_bodypart_xp(bodypart)
+        if xp_before is None:
+            return -1 
+        level = self.body_xp.get_bodypart_level(bodypart)
+        self.body_xp.set_bodypart_xp(bodypart, xp_before + xp)
+        level_after = self.body_xp.get_bodypart_level(bodypart)
+        
+        if level_after > level:
+            return level_after
+        return 0
+        
+    def decrease_sleep_streak(self) -> None:
+        self.sleep_streak = max(self.sleep_streak - 1, 0)
+    
+    def increase_sleep_streak(self) -> None:
+        self.sleep_streak += 1
+    
+    def update_highest_weight(self, weight:int):
+        self.highest_weight = weight
+    
+    
+        
---
+++ tests/test_user.py
@@ -0,0 +1,146 @@
+import unittest
+
+from src.user import User
+from src.bodyparts import BodyParts
+class TestUser(unittest.TestCase):
+    def test_user(self):
+        bodyParts = ["cardio", "back", "lower arm"]
+        bodypart_xp = BodyParts(bodyParts)
+        
+        user:User = User(1, "Filip", 0, bodypart_xp, 0, 2, 0, 6)
+        
+        self.assertEqual(user.id, 1)
+        self.assertEqual(user.name, "Filip")
+        self.assertEqual(user.global_xp, 0)
+        self.assertEqual(user.body_xp.get_bodypart_xp("back"), 0)
+        self.assertEqual(user.streak, 0)
+        self.assertEqual(user.sleep_streak, 2)
+        self.assertEqual(user.log_id, 0)
+        self.assertEqual(user.highest_weight, 6)
+    
+    def test_global_xp(self):
+        bodyParts = ["cardio", "back", "lower arm"]
+        bodypart_xp = BodyParts(bodyParts)
+        
+        user:User = User(1, "Louise", 0, bodypart_xp, 0, 2, 0, 6)
+        
+        level = user.increase_global_xp(200)
+        
+        self.assertEqual(user.id, 1)
+        self.assertEqual(user.name, "Louise")
+        self.assertEqual(user.global_xp, 200)
+        self.assertEqual(user.body_xp.get_bodypart_xp("lower arm"), 0)
+        self.assertEqual(user.streak, 0)
+        self.assertEqual(user.sleep_streak, 2)
+        self.assertEqual(user.log_id, 0)
+        self.assertEqual(user.highest_weight, 6)
+        self.assertEqual(level, 4)
+    
+    
+    def test_body_xp(self):
+        bodyParts = ["cardio", "back", "lower arm"]
+        bodypart_xp = BodyParts(bodyParts)
+        
+        user:User = User(1, "Louise", 0, bodypart_xp, 0, 2, 0, 6)
+        
+        level = user.increase_body_xp("back", 200)
+        
+        self.assertEqual(user.id, 1)
+        self.assertEqual(user.name, "Louise")
+        self.assertEqual(user.global_xp, 0)
+        self.assertEqual(user.body_xp.get_bodypart_xp("back"), 200)
+        self.assertEqual(user.streak, 0)
+        self.assertEqual(user.sleep_streak, 2)
+        self.assertEqual(user.log_id, 0)
+        self.assertEqual(user.highest_weight, 6)
+        self.assertEqual(level, 3)
+    
+    def test_body_nonexisting(self):
+        bodyParts = ["cardio", "back", "lower arm"]
+        bodypart_xp = BodyParts(bodyParts)
+        
+        user:User = User(1, "Louise", 0, bodypart_xp, 0, 2, 0, 6)
+        
+        level = user.increase_body_xp("shoulder", 200)
+        
+        self.assertEqual(user.id, 1)
+        self.assertEqual(user.name, "Louise")
+        self.assertEqual(user.global_xp, 0)
+        self.assertEqual(user.body_xp.get_bodypart_xp("back"), 0)
+        self.assertEqual(user.streak, 0)
+        self.assertEqual(user.sleep_streak, 2)
+        self.assertEqual(user.log_id, 0)
+        self.assertEqual(user.highest_weight, 6)
+        self.assertEqual(level, -1)
+    
+    def test_body_non_level_up(self):
+        bodyParts = ["cardio", "back", "lower arm"]
+        bodypart_xp = BodyParts(bodyParts)
+        
+        user:User = User(1, "Louise", 0, bodypart_xp, 0, 2, 0, 6)
+        
+        level = user.increase_body_xp("back", 10)
+        
+        self.assertEqual(user.id, 1)
+        self.assertEqual(user.name, "Louise")
+        self.assertEqual(user.global_xp, 0)
+        self.assertEqual(user.body_xp.get_bodypart_xp("back"), 10)
+        self.assertEqual(user.streak, 0)
+        self.assertEqual(user.sleep_streak, 2)
+        self.assertEqual(user.log_id, 0)
+        self.assertEqual(user.highest_weight, 6)
+        self.assertEqual(level, 0)
+    
+    def test_sleep_streak(self):
+        bodyParts = ["cardio", "back", "lower arm"]
+        bodypart_xp = BodyParts(bodyParts)
+        
+        user:User = User(1, "Josefin", 0, bodypart_xp, 0, 2, 0, 6)
+        
+        level = user.increase_body_xp("back", 200)
+        
+        self.assertEqual(user.id, 1)
+        self.assertEqual(user.name, "Josefin")
+        self.assertEqual(user.global_xp, 0)
+        self.assertEqual(user.body_xp.get_bodypart_xp("back"), 200)
+        self.assertEqual(user.streak, 0)
+        self.assertEqual(user.sleep_streak, 2)
+        self.assertEqual(user.log_id, 0)
+        self.assertEqual(user.highest_weight, 6)
+        self.assertEqual(level, 3)
+        
+        
+        user.increase_sleep_streak()
+        self.assertEqual(user.sleep_streak, 3)
+        
+        user.decrease_sleep_streak()
+        self.assertEqual(user.sleep_streak, 2)
+        
+        user.decrease_sleep_streak()
+        user.decrease_sleep_streak()
+        user.decrease_sleep_streak()
+        user.decrease_sleep_streak()
+        self.assertEqual(user.sleep_streak, 0)
+    
+    def test_update_highest_weight(self):
+        bodyParts = ["cardio", "back", "lower arm"]
+        bodypart_xp = BodyParts(bodyParts)
+        
+        user:User = User(1, "Louise", 0, bodypart_xp, 0, 2, 0, 6)
+        
+        level = user.increase_body_xp("back", 200)
+        
+        self.assertEqual(user.id, 1)
+        self.assertEqual(user.name, "Louise")
+        self.assertEqual(user.global_xp, 0)
+        self.assertEqual(user.body_xp.get_bodypart_xp("back"), 200)
+        self.assertEqual(user.streak, 0)
+        self.assertEqual(user.sleep_streak, 2)
+        self.assertEqual(user.log_id, 0)
+        self.assertEqual(user.highest_weight, 6)
+        self.assertEqual(level, 3)
+        
+        user.update_highest_weight(15)
+        self.assertEqual(user.highest_weight, 15)
+    
+        
