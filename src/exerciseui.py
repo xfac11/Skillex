6bd33393b32fc1138358889690cb5a29c3facf09
xfac11 <xfac115@gmail.com>
2026-04-09 11:39:44 +0200

    fix #22 The exercise UI is now fixed and no longer shows the images cause the copyright is not open and enhanced with spacing for instructions

--- src/exerciseui.py
+++ src/exerciseui.py
@@ -7,6 +7,7 @@ class ExerciseUI:
         self.exercise = exercise
         
     def to_string(self) -> str:
+        base_url = "https://raw.githubusercontent.com/yuhonas/free-exercise-db/main/exercises/"
         dict_str = ExerciseCatalog().get_exercise_raw_json(self.exercise.id)
         data = ast.literal_eval(dict_str)
         string = ""
@@ -14,8 +15,10 @@ class ExerciseUI:
         string += f"Body part: {self.exercise.bodypart.capitalize()}\n"
         string += f"Target muscle: {self.exercise.target_muscle.capitalize()}\n"
         string += f"Equipment: {self.exercise.equipment.capitalize()}\n"
-        string += f"Link to gif: {data["gifUrl"]}\n"
         string += f"Instructions:\n"
+        step_index = 1
         for step in data["instructions"]:
-            string += f"    {step}\n"
+            string += f"    Step {str(step_index)}: "
+            step_index += 1
+            string += f"{step}\n\n"
         return string
